be256b3c75d2d51f0811ffa5028e1874bdb42444
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2025-02-16 11:51:35 +0530

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Cactro Polls",
+  description: "Chaitanya's Cactro Polling App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+export enum Route {
+  Index = "/",
+  Polls = "/polls",
+  Poll = "/polls/:id",
+  CreatePoll = "/polls/create",
+}
